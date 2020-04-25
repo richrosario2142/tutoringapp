ce9718861eb74e80c5154a2974b5b7b0fd26a50a
Richard Rosario <richard.rosario2142@gmail.com>
2020-04-25 11:56:37 -0400

    Created and modified db schema (student table)

--- backend/scheduler/models.py
+++ backend/scheduler/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=36)
+    last_name = models.CharField(max_length=36)
+    email = models.CharField(max_length=36)
+    subject = models.CharField(max_length=9)
+
